7e2a8e41a68f50c22be78e2ff61b954d13c2d726
christopheylp <christopheyalap957@gmail.com>
2021-07-03 15:43:28 +0200

    Added component and Routes for connection and inscription component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+
+const routes: Routes = [
+  { path: 'connection-component', component: ConnectionComponent },
+  { path: 'inscription-component', component: InscriptionComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    ConnectionComponent,
+    InscriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
