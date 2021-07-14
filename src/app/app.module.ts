eafb6f8a464964fb6507cb242bab6702c06c38dc
christopheylp <christopheyalap957@gmail.com>
2021-07-14 15:03:14 +0200

    Updated connection module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ConnectionComponent } from './connection/connection.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import {FormsModule} from "@angular/forms";
 
 const routes: Routes = [
   { path: 'connection-component', component: ConnectionComponent },
@@ -23,7 +25,9 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule
   ],
   exports: [RouterModule],
   providers: [],
--- src/app/connection/connection.component.ts
+++ src/app/connection/connection.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
-declare var $: any;
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-connection',
@@ -10,20 +10,31 @@ declare var $: any;
 
 export class ConnectionComponent implements OnInit {
 
-  constructor() { }
+  readonly ROOT_URL = ''
+  responseJson: any
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+  }
 
-    // $( "#send-connection-button" ).click(function(){
-    //   $.post("demo_test_post.asp",
-    //     {
-    //       name: "Donald Duck",
-    //       city: "Duckburg"
-    //     },
-    //     function(){
-    //       console.log("Requete envoyé");
-    //     });
-    // });
+  onSubmit(connectionForm: NgForm) {
+    const data = {
+      email: connectionForm.value.email,
+      password: connectionForm.value.password
+    }
+    console.log(data);
+    this.connection(data)
+  }
+
+  connection(data: Object) {
+    this.responseJson = this.http.post(this.ROOT_URL, data)
+    if (this.responseJson.access == "true") {
+      console.log("Accès validé")
+    } else {
+      console.log("Accès refusé")
+    }
 
   }
 
